1e664a42fb57d28e85764b31897754de6bad4e58
FancyKillerPanda <fancykillerpanda@gmail.com>
2022-11-06 10:40:10 +1100

    Added build system.

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "common.hpp"
+
+s32 main(s32 argc, u8* argv[]) {
+	printf("Hello, Infecdead!\n");
+	return 0;
+}
---
+++ tools/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+originalDir=$PWD
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+projectRoot=$scriptDir/..
+
+cd $projectRoot/bin && rm ./Infecdead
+
+mkdir $projectRoot/build >/dev/null 2>/dev/null
+cd $projectRoot/build
+
+cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ ..
+cmake --build .
+
+echo
+cd $projectRoot/bin
+./Infecdead
+
+cd $originalDir

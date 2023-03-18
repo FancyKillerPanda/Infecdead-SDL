9801e3fb72ee3beff08c8dac932d325c0411cd18
FancyKillerPanda <fancykillerpanda@gmail.com>
2023-03-19 10:15:13 +1100

    Updated Camera character targeting.

--- src/game/camera.cpp
+++ src/game/camera.cpp
@@ -8,10 +8,8 @@ Camera::Camera(const Tilemap& tilemap, const Character& characterToFollow)
 
 void Camera::update() {
     glm::vec2 viewport = { VIEWPORT_WIDTH, VIEWPORT_HEIGHT };
-    glm::vec2 worldSpaceViewport = viewport / tilemap.get_default_scale();
-    glm::vec2 mapDimensionsWorldSpace = tilemap.get_map_dimensions() * tilemap.get_tileset().get_tile_dimensions();
+    glm::vec2 worldSpaceViewport = (viewport / tilemap.get_default_scale()) / tilemap.get_tileset().get_tile_dimensions();
 
     targetWorldPosition = characterToFollow.get_world_position();
-    // printf("%f, %f\n", targetWorldPosition.x, targetWorldPosition.y);
-    targetWorldPosition = glm::clamp(targetWorldPosition, worldSpaceViewport / 2.0f, mapDimensionsWorldSpace - (worldSpaceViewport / 2.0f));
+    targetWorldPosition = glm::clamp(targetWorldPosition, worldSpaceViewport / 2.0f, tilemap.get_map_dimensions() - (worldSpaceViewport / 2.0f));
 }
